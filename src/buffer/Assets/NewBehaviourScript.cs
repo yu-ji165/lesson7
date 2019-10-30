aab12c2d3bde620b1f77fd8c2c250cf52c1f4bea
yuji165 <nietononoshana140@icloud.com>
2019-10-30 18:57:29 +0900

    提出してみました

---
+++ src/buffer/Assets/NewBehaviourScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Camera cam = Camera.main;
+        transform.parent = cam.transform;
+        float distance = cam.nearClipPlane + 3.0f;
+        transform.localPosition = new Vector3(0, 0, distance);
+        transform.localRotation = new Quaternion();
+        float inv=(float)Screen.width/Screen.height;
+        float s = Mathf.Tan(0.5f * cam.fieldOfView * Mathf.Deg2Rad) * 2.0f * distance;
+        transform.localScale = new Vector3(inv * s, s, 1);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
